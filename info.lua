130adffb1e8f1c563117aaa4658b86991f5f2ba6
gagehelton <gagehelton@gmail.com>
2023-07-12 15:24:19 -0400

    init

---
+++ info.lua
@@ -0,0 +1,8 @@
+PluginInfo = {
+  Name = "Base Plugin",
+  Version = "0.0",
+  BuildVersion = "2.0.0.2",
+  Id = "322cb95e-a012-430e-aefb-03f5b8f9b7db",
+  Author = "Your_Company",
+  Description = "A very basic plugin"  
+}
