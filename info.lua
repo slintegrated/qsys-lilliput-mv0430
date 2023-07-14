83c55f4f8fa23a51d937e4d8a712a404cb1edeb2
gagehelton <gagehelton@gmail.com>
2023-07-14 13:28:12 -0400

    fix version number

--- info.lua
+++ info.lua
@@ -1,6 +1,6 @@
 PluginInfo = {
   Name = "Lilliput MV0430",
-  Version = "0.0",
+  Version = "0.1",
   BuildVersion = "0.1.0.0",
   Id = "322cb95e-a012-430e-aefb-03f5b8f9b7db",
   Author = "Gage Helton",
